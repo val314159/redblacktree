2b0a2f243c041c493255aaa2dbe6fd3b873bb9ba
@val314159 <jmward@gmail.com>
2021-08-18 20:38:28 -0500

    update

--- src/redblack.c
+++ src/redblack.c
@@ -1,22 +1,23 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define SI static inline
+enum nodeColor{ RED, BLACK };
 
-enum nodeColor{ RED = 0, BLACK = 1 };
+#define true  1
+#define false 0
 
-struct rbNode{
+struct rbNode {
   int data, color;
   struct rbNode *link[2];
 };
 
-struct rbTree{
+struct rbTree {
   int size;
   struct rbNode *root;
 };
 
-SI struct rbNode *createNode(int data, int color){
+struct rbNode *createNode(int data, int color) {
   struct rbNode *newnode;
   newnode = (struct rbNode *)malloc(sizeof(struct rbNode));
   newnode->data = data;
@@ -25,259 +26,279 @@ SI struct rbNode *createNode(int data, int color){
   return newnode;
 }
 
-int insertion(struct rbTree *t, int data){
-  int h = ((int)log2(t->size+1))<<1;
-  int d[h]; struct rbNode *s[h], *p = t->root;
-  if (p == NULL){
-    t->root = createNode(data, BLACK);
+int insertion(struct rbTree *tree,
+	       int data) { 
+  int ht = ((int)log2(tree->size+1))<<1;
+  struct rbNode *stack[ht], *p, *x, *y;
+  int d[ht];
+
+  if (!tree->root) {
+    tree->root = createNode(data, BLACK);
+    tree->size = 1;
     return 1;
   }
-  s[0] = p; d[0] = 0;
-  for(h = 1; ; h++){
-    if (p->data == data){
+  ht = 1;
+  stack[0] = p = tree->root;
+  d[0] = 0;
+  while (1) {
+    if (p->data == data) {
       printf("Duplicates Not Allowed!!\n");
       return 0;
     }
     int n = (data > p->data) ? 1 : 0;
-    s[h] = p; d[h] = n;
+    stack[ht] = p;
+    d[ht++] = n;
     p = p->link[n];
-    if (p == NULL){
-      s[h++]->link[n] = createNode(data, RED);
+    if(p == NULL){
+      stack[ht - 1]->link[n] = createNode(data, RED);
       break;
     }
   }
-
-  struct rbNode *x, *y;
-  while ((h >= 3) && (s[h - 1]->color == RED)){
-    if (d[h - 2] == 0){
-      y = s[h - 2]->link[1];
-      if (y != NULL && y->color == RED){
-        s[--h]->color = y->color = BLACK;
-        s[--h]->color = RED;
+  while ((ht >= 3) && (stack[ht - 1]->color == RED)) {
+    if (d[ht - 2] == 0) {
+      y = stack[ht - 2]->link[1];
+      if (y != NULL && y->color == RED) {
+        stack[--ht]->color = RED;
+        stack[--ht]->color = y->color = BLACK;
 	continue;
       }
-      if (d[h - 1] == 0){
-	y = s[h - 1];
-      } else{
-	x = s[h - 1];
-	y = s[h - 2]->link[0] = x->link[1];
+      if (d[ht - 1] == 0) {
+	y = stack[ht - 1];
+      } else {
+	x = stack[ht - 1];
+	stack[ht - 2]->link[0] = y = x->link[1];
 	x->link[1] = y->link[0]; y->link[0] = x;
       }
-      ;;x = s[h - 2];
-      ;;x->link[0] = y->link[1]; y->link[1] = x;
-    } else{
-      y = s[h - 2]->link[0];
-      if ((y != NULL) && (y->color == RED)){
-        s[--h]->color = RED;
-        s[--h]->color = y->color = BLACK;
+      x = stack[ht - 2];
+      x->link[0] = y->link[1]; y->link[1] = x;
+    } else {
+      y = stack[ht - 2]->link[0];
+      if ((y != NULL) && (y->color == RED)) {
+        stack[--ht]->color = RED;
+        stack[--ht]->color = y->color = BLACK;
 	continue;
       }
-      if (d[h - 1] == 1){
-	y = s[h - 1];
-      } else{
-	x = s[h - 1];
-	y = s[h - 2]->link[1] = x->link[0];
+      if (d[ht - 1] == 1) {
+	y = stack[ht - 1];
+      } else {
+	x = stack[ht - 1];
+	stack[ht - 2]->link[1] = y = x->link[0];
 	x->link[0] = y->link[1]; y->link[1] = x;
       }
-      ;;x = s[h - 2];
-      ;;x->link[1] = y->link[0]; y->link[0] = x;
+      x = stack[ht - 2];
+      x->link[1] = y->link[0]; y->link[0] = x;
     }
     x->color = RED;
     y->color = BLACK;
-    if    (x == t->root)  t->root = y;
-    else s[h - 3]->link[d[h - 3]] = y;
+    if (x == tree->root) {
+      tree->root = y;
+    } else {
+      stack[ht - 3]->link[d[ht - 3]] = y;
+    }
     break;
   }
-  if(t->root->color == RED){
-    printf("_ROOT IS RED, MEANS RUN SON! "
-	   "NUMBERS ADD UP TO NOTHIN\n");
+  //root->color = BLACK;
+  if(tree->root->color == RED){
+    printf("WRONG COLOR IN ROOT\n");
     exit(1);
-  }
-  t->size++;
+  }    
+  tree->size++;
   return 1;
 }
 
-int deletion(struct rbTree *t, int data){
-  int h = ((int)log2(t->size+1))<<1;
-  int d[h]; struct rbNode *s[h], *p = t->root;
-  if (p == NULL){
-    printf("Data not available\n");
-    return 0;
-  }
+int deletion(struct rbTree *tree,
+	      int data) {
+  int ht = ((int)log2(tree->size+1))<<1;
+  struct rbNode *stack[ht], *p, *x, *y;
+  int d[ht];
+  ht = 0;
+  p = tree->root;
   
-  for (h = 0; ; h++){
-    if (data == p->data){
-      ;
-      break;
-    }
-    int n = (data > p->data) ? 1 : 0;
-    s[h] = p; d[h] = n;
-    p = p->link[n]; 
-    if (p == NULL){
+  while (1) {
+    if (p == NULL) {
       printf("Data not available\n");
       return 0;
     }
+    if (data == p->data)
+      break;
+    int n = (data > p->data) ? 1 : 0;
+    stack[ht] = p;
+    d[ht++] = n;
+    p = p->link[n];
   }
- 
-  struct rbNode *x, *y;
+
   x = p->link[1];
-  if (x == NULL){
-    if (p == t->root){
-      t->root = p->link[0];
-    } else{
-      s[h - 1]->link[d[h - 1]] = p->link[0];
+  if (x == NULL) {
+    if (p == tree->root) {
+      tree->root = p->link[0];
+    } else {
+      stack[ht - 1]->link[d[ht - 1]] = p->link[0];
     }
-  } else if (x->link[0] == NULL){
+  } else if (x->link[0] == NULL) {
     x->link[0] = p->link[0];
-    if (x->color == RED){
-      if(p->color == BLACK){
-	x->color = BLACK;
-	p->color = RED;
-      }
-    }else{
-      if(p->color == RED){
-	x->color = RED;
-	p->color = BLACK;
-      }
+
+    enum nodeColor color;
+    color = x->color;
+    x->color = p->color;
+    p->color = color;
+
+    if (p == tree->root) {
+      tree->root = x;
+    } else {
+      stack[ht - 1]->link[d[ht - 1]] = x;
     }
-    if (p == t->root){
-      t->root = x;
-    } else{
-      s[h - 1]->link[d[h - 1]] = x;
-    }
-    d[h] = 1; s[h] = x; h++;
-  } else{
-    int i = h++;
-    while(1){
-      d[h] = 0; s[h] = x; h++;
+
+    d[ht] = 1;
+    stack[ht++] = x;
+  } else {
+    int i = ht++;
+    while (1) {
+      d[ht] = 0;
+      stack[ht++] = x;
       y = x->link[0];
-      if (y->link[0] == NULL)
+      if (!y->link[0])
 	break;
       x = y;
     }
-    d[i] = 1; s[i] = y;
+
+    d[i] = 1;
+    stack[i] = y;
     if (i > 0)
-      s[i - 1]->link[d[i - 1]] = y;
+      stack[i - 1]->link[d[i - 1]] = y;
+
     y->link[0] = p->link[0];
+
     x->link[0] = y->link[1];
     y->link[1] = p->link[1];
-    if (p == t->root){
-      t->root = y;
+
+    if (p == tree->root) {
+      tree->root = y;
     }
-    if (y->color == RED){
-      if(p->color == BLACK){
-	y->color = BLACK;
-	p->color = RED;
-      }
-    }else{
-      if(p->color == RED){
-	y->color = RED;
-	p->color = BLACK;
-      }
-    }
+
+    enum nodeColor color;
+    color = y->color;
+    y->color = p->color;
+    p->color = color;
   }
-  if (h > 0 && p->color == BLACK){
-    while (1){
-      p = s[h - 1]->link[d[h - 1]];
-      if (p != NULL && p->color == RED){
+
+  if (ht > 0 && p->color == BLACK) {
+    while (1) {
+      p = stack[ht - 1]->link[d[ht - 1]];
+      if (p && p->color == RED) {
         p->color = BLACK;
         break;
       }
-      if (h < 2)
+
+      if (ht < 2)
         break;
-      if (d[h - 2] == 0){
-        y = s[h - 1]->link[1];
-        if (y == NULL)
+
+      if (d[ht - 2] == 0) {
+        y = stack[ht - 1]->link[1];
+
+        if (!y)
           break;
-        if (y->color == RED){
-          s[h - 1]->color = RED;
+
+        if (y->color == RED) {
+          stack[ht - 1]->color = RED;
           y->color = BLACK;
-          s[h - 1]->link[1] = y->link[0];
-          y->link[0] = s[h - 1];
-          if (s[h - 1] == t->root){
-            t->root = y;
-          } else{
-            s[h - 2]->link[d[h - 2]] = y;
+          stack[ht - 1]->link[1] = y->link[0];
+          y->link[0] = stack[ht - 1];
+
+          if (stack[ht - 1] == tree->root) {
+            tree->root = y;
+          } else {
+            stack[ht - 2]->link[d[ht - 2]] = y;
           }
-          d[h] = 0; s[h] = s[h - 1]; s[h - 1] = y; h++;
-          y = s[h - 1]->link[1];
+          d[ht] = 0;
+          stack[ht] = stack[ht - 1];
+          stack[ht - 1] = y;
+          ht++;
+
+          y = stack[ht - 1]->link[1];
         }
-        if ((y->link[0]==NULL || y->link[0]->color == BLACK) &&
-	    (y->link[1]==NULL || y->link[1]->color == BLACK)){
+
+        if ((!y->link[0] || y->link[0]->color == BLACK) &&
+	    (!y->link[1] || y->link[1]->color == BLACK)) {
           y->color = RED;
-        } else{
-          if(y->link[1]==NULL || y->link[1]->color == BLACK){
+        } else {
+          if(!y->link[1] || y->link[1]->color == BLACK) {
             x = y->link[0];
             y->color = RED;
             x->color = BLACK;
             y->link[0] = x->link[1];
             x->link[1] = y;
-            y = s[h - 1]->link[1] = x;
+            y = stack[ht - 1]->link[1] = x;
           }
-          y->color = s[h - 1]->color;
-          s[h - 1]->color = BLACK;
+          y->color = stack[ht - 1]->color;
+          stack[ht - 1]->color = BLACK;
           y->link[1]->color = BLACK;
-          s[h - 1]->link[1] = y->link[0];
-          y->link[0] = s[h - 1];
-          if (s[h - 1] == t->root){
-            t->root = y;
-          } else{
-            s[h - 2]->link[d[h - 2]] = y;
+          stack[ht - 1]->link[1] = y->link[0];
+          y->link[0] = stack[ht - 1];
+          if (stack[ht - 1] == tree->root) {
+            tree->root = y;
+          } else {
+            stack[ht - 2]->link[d[ht - 2]] = y;
           }
           break;
         }
-      } else{
-        y = s[h - 1]->link[0];
-        if (y == NULL)
+      } else {
+        y = stack[ht - 1]->link[0];
+        if (!y)
           break;
-        if (y->color == RED){
-          s[h - 1]->color = RED;
+
+        if (y->color == RED) {
+          stack[ht - 1]->color = RED;
           y->color = BLACK;
-          s[h - 1]->link[0] = y->link[1];
-          y->link[1] = s[h - 1];
-          if (s[h - 1] == t->root){
-            t->root = y;
-          } else{
-            s[h - 2]->link[d[h - 2]] = y;
+          stack[ht - 1]->link[0] = y->link[1];
+          y->link[1] = stack[ht - 1];
+
+          if (stack[ht - 1] == tree->root) {
+            tree->root = y;
+          } else {
+            stack[ht - 2]->link[d[ht - 2]] = y;
           }
-          d[h] = 1; s[h] = s[h - 1]; s[h - 1] = y; h++;
-	  y = s[h - 2]->link[0];
+          d[ht] = 1;
+          stack[ht] = stack[ht - 1];
+          stack[ht - 1] = y;
+          ht++;
+
+          y = stack[ht - 1]->link[0];
         }
-        if ((y->link[0]==NULL || y->link[0]->color == BLACK) &&
-	    (y->link[1]==NULL || y->link[1]->color == BLACK)){
+        if ((!y->link[0] || y->link[0]->color == BLACK) &&
+	    (!y->link[1] || y->link[1]->color == BLACK)) {
           y->color = RED;
-        } else{
-          if(y->link[0]==NULL || y->link[0]->color == BLACK){
+        } else {
+          if(!y->link[0] || y->link[0]->color == BLACK) {
             x = y->link[1];
             y->color = RED;
             x->color = BLACK;
             y->link[1] = x->link[0];
             x->link[0] = y;
-            y = s[h - 1]->link[0] = x;
+            y = stack[ht - 1]->link[0] = x;
           }
-          y->color = s[h - 1]->color;
-          s[h - 1]->color = BLACK;
+          y->color = stack[ht - 1]->color;
+          stack[ht - 1]->color = BLACK;
           y->link[0]->color = BLACK;
-          s[h - 1]->link[0] = y->link[1];
-          y->link[1] = s[h - 1];
-          if (s[h - 1] == t->root){
-            t->root = y;
-          } else{
-            s[h - 2]->link[d[h - 2]] = y;
+          stack[ht - 1]->link[0] = y->link[1];
+          y->link[1] = stack[ht - 1];
+          if (stack[ht - 1] == tree->root) {
+            tree->root = y;
+          } else {
+            stack[ht - 2]->link[d[ht - 2]] = y;
           }
           break;
         }
       }
-      h--;
+      ht--;
     }
   }
   free(p);
-  t->size--;
+  tree->size--;
   return 1;
 }
 
-void inorderTraversal(struct rbNode *node){
-  if (node != NULL){
+void inorderTraversal(struct rbNode *node) {
+  if (node) {
     inorderTraversal(node->link[0]);
     printf("%d, ", node->data);
     inorderTraversal(node->link[1]);
@@ -285,30 +306,27 @@ void inorderTraversal(struct rbNode *node){
 }
 
 void driver(){
-  struct rbTree tree ={ 0, NULL };
-  char buf[16];
-  while(fgets(buf, sizeof(buf), stdin)){
-    switch (buf[0]){
+  struct rbTree tree;
+  char b[12];
+  while(fgets(b,sizeof(b),stdin)){
+    switch(b[0]){
     case '+':
-      insertion(&tree, atoi(buf+1));
+      insertion(&tree, atoi(b+1));
       break;
     case '-':
-      ;deletion(&tree, atoi(buf+1));
+      ;deletion(&tree, atoi(b+1));
       break;
-    case'\r':
     case'\n':
+    case'\r':
       inorderTraversal(tree.root);
       printf("\n");
       break;
-    case 'x':
-      exit(0);
     default:
-      printf("Not available\n");
-      break;
+      printf("unknown: [%s]\n", b);
     }
   }
 }
 
-int main(){
+int main() {
   driver();
 }
